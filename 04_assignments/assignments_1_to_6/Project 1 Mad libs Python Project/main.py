4f7cc29a18820020f6f7c145569afe2e25cbed5b
arsalan4work <m.arsalan4210@gmail.com>
2025-03-30 15:22:29 +0500

    add files

---
+++ 04_assignments/assignments_1_to_6/Project 1 Mad libs Python Project/main.py
@@ -0,0 +1,11 @@
+print(f"""Fill out the Following: Computer programming is so _____! It's make me so excited all the time because
+    I love to _____. Stay hyderated and _____ like you are _____""")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"""Computer programming is so {adj}! It's make me so excited all the time because
+    I love to {verb1}. Stay hyderated and {verb2} like you are {famous_person}"""
+print(madlib)
