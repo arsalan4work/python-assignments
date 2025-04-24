de93a1c5791d1af0ef1c70b9a38b5f6725e56316
arsalan4work <m.arsalan4210@gmail.com>
2025-04-24 16:05:06 +0500

    add files

--- 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP/12.py
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP/12.py
@@ -1,3 +1,23 @@
 # 12. Static Methods
 # Assignment:
 # Create a class TemperatureConverter with a static method celsius_to_fahrenheit(c) that returns the Fahrenheit value.
+
+# class TemperatureConverter
+class TemperatureConverter:
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    # Static method
+    @staticmethod
+    def celsius_to_fahrenheit(c):
+        # Formula
+        return (c * 9/5) + 32
+    
+# Initialize value
+celcius = 32
+# storing data
+fahrenheit = TemperatureConverter.celsius_to_fahrenheit(celcius)
+# printing data
+print(f"{celcius}°C is equal to {fahrenheit}°F")
+
+# Output: 32°C is equal to 89.6°F
