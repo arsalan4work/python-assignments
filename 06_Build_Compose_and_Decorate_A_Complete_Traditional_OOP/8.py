66f9a17e7818314b8847fe9974b08ae1b1179ffd
arsalan4work <m.arsalan4210@gmail.com>
2025-04-24 15:00:16 +0500

    add files

--- 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP/8.py
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP/8.py
@@ -2,3 +2,19 @@
 # Assignment:
 # Create a class Person with a constructor that sets the name. Inherit a class Teacher from it, 
 # add a subject field, and use super() to call the base class constructor.
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class Teacher(Person):
+    def __init__(self, name, subject):
+        super().__init__(name)
+        self.subject = subject
+
+    def display(self):
+        return f"Teacher name: {self.name}, Subject: {self.subject}"
+
+student = Teacher("Arsalan", "DSA")
+print(student.display())
