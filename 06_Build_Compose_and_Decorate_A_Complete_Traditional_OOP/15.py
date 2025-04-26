c142fc4e66dacd0d687d956cca5a3bdbadf8ad93
arsalan4work <m.arsalan4210@gmail.com>
2025-04-26 18:38:09 +0500

    add files

--- 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP/15.py
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP/15.py
@@ -9,3 +9,24 @@
 # - D that inherits from both B and C.
 
 # Create an object of D and call show() to observe MRO.
+
+class A:
+    def show(self):
+        print("Method from class A")
+
+class B(A):
+    def show(self):
+        print("Method from class B")
+
+class C(A):
+    def show(self):
+        print("Method from class C")
+
+class D(B, C):
+    pass
+
+d = D()
+# Calling the show() method to observe the method resulution order(MRO)
+d.show()
+# Printing the MRO to observe the order
+print(D.mro())
